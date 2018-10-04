5cb931a9822473957940499b438caedf71c803a4
mimancino <mimancino@student.mamkschools.org>
2018-10-04 10:53:06 -0400

    SUBMIT V2

--- src/MichaelMancinoRandomHatV2.java
+++ src/MichaelMancinoRandomHatV2.java
@@ -5,6 +5,7 @@ public class MichaelMancinoRandomHatV2 {
         start();
     }
     public static void start(){
+        //Initalizes scanner and makes sure the input is an even number
         Scanner scan = new Scanner(System.in);
         int amt = 1;
         do{
@@ -13,6 +14,7 @@ public class MichaelMancinoRandomHatV2 {
 
         }while(amt% 2 !=0);
         int[] teams = new int[amt];
+        //generates an array of whatever length was asked by the user
         for(int i = 0; i <teams.length;i++){
             teams[i] = i+1;
         }
@@ -22,6 +24,7 @@ public class MichaelMancinoRandomHatV2 {
         int pick = -1;
         int pick2 = -1;
         for(int i = 0; i<teams.length/2;i++){
+            //generates a random number, and if the number isn't unique, generate it again
             while(pick == -1) {
                 pick = (int)(Math.random() * (teams.length));
                 pick = teams[pick];
@@ -32,7 +35,7 @@ public class MichaelMancinoRandomHatV2 {
                 pick2 =  (int)(Math.random()*(teams.length));
                 pick2 = teams[pick2];
             }
-
+            //tells what two teams will be battling and sets whatever teams were used to -1 so they are not called again
             System.out.println("Teams " + pick + " and " + pick2 + " will battle.");
             teams[pick-1] = -1;
             teams[pick2-1] = -1;
