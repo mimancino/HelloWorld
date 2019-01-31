4422c65facc89a1b014463bb6c95c057733644b7
mimancino <mimancino@student.mamkschools.org>
2019-01-31 09:51:59 -0500

    bad

--- src/Ch10ArrayList/Vocabulary1.java
+++ src/Ch10ArrayList/Vocabulary1.java
@@ -12,9 +12,12 @@ public static void main(String[]args)throws FileNotFoundException {
         Scanner scan2 = new Scanner(new File("text2.txt"));
         ArrayList<String> list = getWords(scan1);
         ArrayList<String> list2 = getWords(scan2);
+        System.out.println(list);
+    System.out.println(list2);
 
 
         }
+        //This method reads the data of an external file and converts it to lowercase and applies casefolding and removes any duplicate words
         public static ArrayList<String> getWords(Scanner scan){
             //Read all words + Sort
             ArrayList<String> words = new ArrayList<>();
@@ -22,6 +25,39 @@ public static void main(String[]args)throws FileNotFoundException {
                 words.add(scan.next().toLowerCase());
             }
             Collections.sort(words);
-            return words;
+            //Eliminate duplicates
+            ArrayList<String> uniqueWords = new ArrayList<String>();
+            if(words.size() > 0) {
+                uniqueWords.add(words.get(0));
+            }
+                for (int i = 1; i<words.size();i++){
+                    if (!words.get(i).equals(words.get(i-1))){
+                        uniqueWords.add(words.get(i));
+
+                    }
+
+                }
+
+                    return uniqueWords;
+        }
+        public static double overlap(ArrayList<String> l1, ArrayList<String> l2){
+        int i1 = 0;
+        int i2 = 0;
+        double matches = 0;
+        while(i1 < l1.size()){
+            if(l1.get(i1).equals(l2.get(i2))){
+                i1++;
+                i2++;
+                matches++;
+
+            }
+            else if(l1.get(i1) < (l2.get(i2)){
+                i1++;
+            }
+            else{
+                i2++;
+
+            }        }
+
         }
 }
